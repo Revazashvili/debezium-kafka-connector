2441c9a6ea3cceb37c373f476d06fabda2930c64
Revazashvili <levanrevazashvili@gmail.com>
2025-02-13 18:55:06 +0400

    move logics to separate files

--- producer/main.go
+++ producer/main.go
@@ -19,7 +19,9 @@ func main() {
 	}
 	defer pool.Close()
 
-	err = setupTable(pool, ctx)
+	di := NewDbInitializer(pool)
+	store := NewEventsStore(pool)
+	err = di.Init(ctx)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Unable to setup table: %v\n", err)
 	}
@@ -28,7 +30,7 @@ func main() {
 		time.Sleep(time.Second * 3)
 
 		es := events.GenerateEvents()
-		err = insertOutboxMessages(pool, es, ctx)
+		err = store.Save(es, ctx)
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "Unable to insert outbox messages: %v\n", err)
 			return
@@ -36,78 +38,4 @@ func main() {
 
 		fmt.Println("Successfully inserted outbox messages")
 	}
-}
-
-func insertOutboxMessages(pool *pgxpool.Pool, messages []events.OutboxMessage, ctx context.Context) error {
-	sql := `INSERT INTO outbox.outbox_messages (payload, timestamp, aggregate_id, type) VALUES ($1, $2, $3, $4)`
-
-	conn, err := pool.Acquire(ctx)
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
-	tx, err := conn.Begin(ctx)
-
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback(ctx)
-
-	for _, msg := range messages {
-		_, err := tx.Exec(ctx, sql, msg.Payload, msg.Timestamp, msg.AggregateId, msg.Type)
-		if err != nil {
-			return err
-		}
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-const (
-	createTable = `
-CREATE TABLE IF NOT EXISTS outbox.outbox_messages (
-    id SERIAL PRIMARY KEY,
-    payload jsonb NOT NULL,
-    timestamp TIMESTAMPTZ DEFAULT NOW(),
-    aggregate_id BIGINT NOT NULL,
-    type VARCHAR(255) NOT NULL
-);`
-
-	createSchema = "CREATE SCHEMA IF NOT EXISTS outbox"
-)
-
-func setupTable(pool *pgxpool.Pool, ctx context.Context) error {
-	c, err := pool.Acquire(ctx)
-	if err != nil {
-		return err
-	}
-	defer c.Release()
-
-	tx, err := c.Begin(ctx)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback(ctx)
-
-	_, err = tx.Exec(ctx, createSchema)
-	if err != nil {
-		return err
-	}
-	_, err = tx.Exec(ctx, createTable)
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
